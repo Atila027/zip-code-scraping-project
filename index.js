a9ec6213f59d5941de3361a18ed9e9afd1921341
Atila027 <atila423027@outlook.com>
2023-03-24 12:25:51 +0900

    updated

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const XLSX = require('xlsx')
 const Axios = require('axios').default
 const FormData = require('form-data')
 
+/*--------------------Request URL--------------------*/
 const BASE_URL = "https://israelpost.co.il/umbraco/Surface/Zip/FindZip"
 let addressDataInfo = [];
+
+/*---------------------Read Address file-------------------*/
 const workbook = XLSX.readFile('addresses_data.xlsx');
 const sheet_name_list = workbook.SheetNames;
 const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 // console.log(xlData.length);
+
+/*---------------------Start Row and End Row for addresss-------------------*/
+const startRow = 1;
+const endRow = 10;
 
 const requestZipCode = (city,street,homeNumber) =>{
   var bodyData = new FormData();
@@ -40,6 +47,29 @@ const getAddressData = (xlsxData) =>{
 
 getAddressData(xlData);
 
-for (let index = 1; index < 501; index++) {
-  requestZipCode(xlData[3].__EMPTY,xlData[3].__EMPTY_1,index)
+// for (let index = 1; index < 501; index++) {
+//   requestZipCode(xlData[3].__EMPTY,xlData[3].__EMPTY_1,index)
+// }
+
+for (let addressIndex = startRow; addressIndex < endRow; addressIndex++) {
+  for (let homeNumber = 1; homeNumber <=500; homeNumber++) {
+    let bodyData = new FormData();
+    bodyData.append('StreetID', "");
+    bodyData.append('House', homeNumber);
+    bodyData.append('Entry', "");
+    bodyData.append('City', addressDataInfo[addressIndex].city);
+    bodyData.append('Street', addressDataInfo[addressIndex].street);
+    // bodyData.append('ByMaanimID', true);
+    bodyData.append('__RequestVerificationToken', '8pFf-3UA0Ezy4jE1fiWq9LX9Nb6kV99KHwjcc6uMQrAfULn2KskclwybnHq4aLyn9eC6J7MLoHrix9TCn2XixKlI59C8-AEIhppmCP_1Nsk1');
+    Axios({
+      method  : 'POST',
+      url     : BASE_URL,
+      headers : bodyData.getHeaders(),
+      data    : bodyData
+    }).then((resolve) => {
+        console.log(resolve.data);
+    }).catch((error) => {
+      addressIndex += 1
+    });
+  }  
 }
