efa691015efc9efb96e96a017cf60c14236ae1ac
Atila027 <atila423027@outlook.com>
2023-03-24 14:47:42 +0900

    updated

--- index.js
+++ index.js
@@ -48,31 +48,54 @@ const getAddressData = (xlsxData) =>{
 getAddressData(xlData);
 
 /*---------------------Main run part-------------------*/
-let count = 0;
-for (let addressIndex = startRow; addressIndex < endRow; addressIndex++) {
-  for (let homeNumber = 1; homeNumber <=500;) {
-    let bodyData = new FormData();
-    bodyData.append('House', homeNumber);
-    bodyData.append('Entry', "");
-    bodyData.append('City', xlData[addressIndex].__EMPTY);
-    bodyData.append('Street', xlData[addressIndex].__EMPTY_1);
-    // bodyData.append('ByMaanimID', true);
-    bodyData.append('__RequestVerificationToken', '8pFf-3UA0Ezy4jE1fiWq9LX9Nb6kV99KHwjcc6uMQrAfULn2KskclwybnHq4aLyn9eC6J7MLoHrix9TCn2XixKlI59C8-AEIhppmCP_1Nsk1');
-    Axios({
-      method  : 'POST',
-      url     : BASE_URL,
-      headers : bodyData.getHeaders(),
-      data    : bodyData
-    }).then((resolve) => {
-      if(resolve.data.zip === ''){
+const scrap = async () => {
+  let count = 0;
+  for (let addressIndex = startRow; addressIndex < endRow; addressIndex++) {
+    for (let homeNumber = 1; homeNumber <=500;) {
+      let bodyData = new FormData();
+      bodyData.append('House', homeNumber);
+      bodyData.append('Entry', "");
+      bodyData.append('City', xlData[addressIndex].__EMPTY);
+      bodyData.append('Street', xlData[addressIndex].__EMPTY_1);
+      // bodyData.append('ByMaanimID', true);
+      bodyData.append('__RequestVerificationToken', '8pFf-3UA0Ezy4jE1fiWq9LX9Nb6kV99KHwjcc6uMQrAfULn2KskclwybnHq4aLyn9eC6J7MLoHrix9TCn2XixKlI59C8-AEIhppmCP_1Nsk1');
+      try {
+        const response = await Axios({
+          method  : 'POST',
+          url     : BASE_URL,
+          headers : bodyData.getHeaders(),
+          data    : bodyData
+        });
+
+        if(response.data.zip === '') {
+          break;
+        } else {
+          count ++;
+          console.log([count, xlData[addressIndex].__EMPTY, xlData[addressIndex].__EMPTY_1, homeNumber, resolve.data.zip]);
+          homeNumber += 1;
+        }
+      } catch (e) {
         break;
-      }else{
-         count += 1
-        console.log([count, xlData[addressIndex].__EMPTY, xlData[addressIndex].__EMPTY_1, homeNumber, resolve.data.zip]);
-        homeNumber += 1;
       }
-    }).catch((error) => {
-      break;
-    });
-  }  
+      
+      // Axios({
+      //   method  : 'POST',
+      //   url     : BASE_URL,
+      //   headers : bodyData.getHeaders(),
+      //   data    : bodyData
+      // }).then((resolve) => {
+      //   if(resolve.data.zip === ''){
+      //     break;
+      //   }else{
+      //      count += 1
+      //     console.log([count, xlData[addressIndex].__EMPTY, xlData[addressIndex].__EMPTY_1, homeNumber, resolve.data.zip]);
+      //     homeNumber += 1;
+      //   }
+      // }).catch((error) => {
+      //   break;
+      // });
+    }  
+  }
 }
+
+scrap();
